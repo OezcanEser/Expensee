42bb47432a7595cff4f73581f25cd3fd0cb74090
matijaskorup1991 <smatija1991@gmail.com>
2021-08-17 07:12:50 +0200

    get all on redirect

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const { redirect } = require('../controller/user');
 
 router.route('/auth/google').get(
   passport.authenticate('google', {
@@ -10,9 +11,11 @@ router.route('/auth/google').get(
 
 router
   .route('/auth/google/redirect')
-  .get(passport.authenticate('google'), (req, res) => {
-    console.log('Login success', req.user);
-  });
+  .get(passport.authenticate('google'), redirect);
+
+// (req, res) => {
+//   console.log('Login success', req.user);
+// }
 
 router.route('/logout').get((req, res) => {
   res.send('logout with passport');
