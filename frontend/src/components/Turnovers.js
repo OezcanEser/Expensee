b1da4ccb1b8a3dede4e3cecf258be7f44ab76cd2
OezcanEser <oe.eser84@gmail.com>
2021-08-18 17:08:42 +0200

    new

--- frontend/src/components/Turnovers.js
+++ frontend/src/components/Turnovers.js
@@ -9,10 +9,10 @@ import React from 'react';
 const optionData = ['Kategorie', 'Einnahmen', 'Ausgaben', 'Sonstiges'];
 
 const Turnovers = () => {
-    const [data, setData] = useState("");
-    const [inputs, setInputs] = useState({ category: "", description: "", price: "", created_at: "" })
-    const [error, setError] =useState("")
-    const [open, setOpen] = React.useState(false);
+  const [data, setData] = useState("");
+  const [inputs, setInputs] = useState({ category: "", description: "", price: "", created_at: "" })
+  const [error, setError] = useState("")
+  const [open, setOpen] = React.useState(false);
 
   const handleInputs = (event) => {
     setInputs((prev) => {
@@ -24,151 +24,66 @@ const Turnovers = () => {
     });
   };
 
-    const newTransfer = (event) => {
-        event.preventDefault()
-        axios.post(`/input`, inputs)
-            .then(result => console.log(result))
-            .catch(err => err.response ? setError(err.response.data.message) : setError(error.message))
-    }
+  const newTransfer = (event) => {
+    event.preventDefault()
+    axios.post(`/input`, inputs)
+      .then(result => console.log(result))
+      .catch(err => err.response ? setError(err.response.data.message) : setError(error.message))
+  }
 
-    //Modal
-    
-  
-    const handleClose = () => {
-        setOpen(false);
-    };
-        
-    const handleOpen = () => {
-        setOpen(true);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    return (
-        <>
-            <Header title="Wallet"/>
-            <main>
-                <section className="wallet">
-                    <Link to="/home"><img src="../img/shape.png" alt="#" /></Link>
-                    <h1>Umsätze</h1>
-                    <form onSubmit={ (ev) => newTransfer(ev)}>
-                        <select onChange={(event) => setData(event.target.value)} required>{valueChoice}</select> <br />
-                        <input type="text" name="description" placeholder="Beschreibung" value={inputs.description} onChange={handleInputs} required /> <br />
-                        <input type="number" name="price" placeholder="Geldbetrag" value={inputs.price} onChange={handleInputs} required /> <br />
-                        <input type="date" name="created_at" placeholder="Datum" value={inputs.created_at} onChange={handleInputs} required /> <br />
-                        <input type="submit" value="Abschicken" onClick={handleOpen}/>
-                    </form>
-                    <h1>{error}</h1>
-                    <Modal
-                        onClose={handleClose}
-                        open={open}
-                        style={{
-                        position: 'absolute',
-                        backgroundColor: '#2B2D5B',
-                        height:350,
-                        width: 300,
-                        margin: 'auto',
-                        }}
-                    >
-                    <div id="success">
-                        <img src="../img/sucess.png" alt="" />
-                        <h3><span>Erfolgreich</span><br />eingetragen!</h3>
-                        <img src="./img/line.png"/>
-                        <section>
-                            <article>
-                                <p><span>Datum</span><br />{data.created_at}</p>
-                                <p><span>Zeit</span><br />{data}</p>
-                            </article>
-                            <p><span>Kategorie</span><br />{data.category}</p>
-                            <p><span>Summe</span><br />{data.price}</p>
-                        </section>
-                        
-                    </div>
-                    </Modal>
-                </section>
-            </main>
-            <Footer />
-        </>
-    );
-  });
-
-  const newTransfer = () => {
-    axios
-      .post(`/input`, inputs)
-      .then((result) => (window.location.href = result.data.redirect))
-      .catch((err) => console.log(err));
+  const handleOpen = () => {
+    setOpen(true);
   };
 
   return (
     <>
-      <Header title='Wallet' />
+      <Header title="Wallet" />
       <main>
-        <section>
-          <Link to='/home'>
-            <img src='../img/shape.png' alt='#' />
-          </Link>
+        <section className="wallet">
+          <Link to="/home"><img src="../img/shape.png" alt="#" /></Link>
           <h1>Umsätze</h1>
-          <div id='success'>
-            <img src='../img/sucess.png' alt='' />
-            <h3>
-              <span>Erfolgreich</span>
-              <br />
-              eingetragen!
-            </h3>
-            <p>
-              <span>Datum</span>
-              <br />
-              {data.created_at}
-            </p>
-            <p>
-              <span>Zeit</span>
-              <br />
-              {data}
-            </p>
-            <p>
-              <span>Kategorie</span>
-              <br />
-              {data.category}
-            </p>
-            <h3>
-              <span>Summe</span>
-              <br />
-              {data.price}
-            </h3>
-          </div>
-          <form>
-            <select onChange={(event) => setData(event.target.value)} required>
-              {valueChoice}
-            </select>
-            <input
-              type='text'
-              name='description'
-              placeholder='Beschreibung'
-              value={inputs.description}
-              onChange={handleInputs}
-              required
-            />
-            <input
-              type='number'
-              name='price'
-              placeholder='Geldbetrag'
-              value={inputs.price}
-              onChange={handleInputs}
-              required
-            />
-            <input
-              type='date'
-              name='created_at'
-              placeholder='Datum'
-              value={inputs.created_at}
-              onChange={handleInputs}
-              required
-            />
-            <input type='submit' value='Abschicken' onClick={newTransfer} />
+          <form onSubmit={(ev) => newTransfer(ev)}>
+            <select onChange={(event) => setData(event.target.value)} required>{valueChoice}</select> <br />
+            <input type="text" name="description" placeholder="Beschreibung" value={inputs.description} onChange={handleInputs} required /> <br />
+            <input type="number" name="price" placeholder="Geldbetrag" value={inputs.price} onChange={handleInputs} required /> <br />
+            <input type="date" name="created_at" placeholder="Datum" value={inputs.created_at} onChange={handleInputs} required /> <br />
+            <input type="submit" value="Abschicken" onClick={handleOpen} />
           </form>
+          <h1>{error}</h1>
+          <Modal
+            onClose={handleClose}
+            open={open}
+            style={{
+              position: 'absolute',
+              backgroundColor: '#2B2D5B',
+              height: 350,
+              width: 300,
+              margin: 'auto',
+            }}
+          >
+            <div id="success">
+              <img src="../img/sucess.png" alt="" />
+              <h3><span>Erfolgreich</span><br />eingetragen!</h3>
+              <img src="./img/line.png" />
+              <section>
+                <article>
+                  <p><span>Datum</span><br />{data.created_at}</p>
+                  <p><span>Zeit</span><br />{data}</p>
+                </article>
+                <p><span>Kategorie</span><br />{data.category}</p>
+                <p><span>Summe</span><br />{data.price}</p>
+              </section>
+            </div>
+          </Modal>
         </section>
       </main>
       <Footer />
     </>
-  );
-};
+  )
+}
 
 export default Turnovers;
