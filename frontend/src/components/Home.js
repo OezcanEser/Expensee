36f9bce41922e265500f0694fc9bf4fed6328721
OezcanEser <oe.eser84@gmail.com>
2021-08-18 10:32:10 +0200

    new

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,47 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+import axios from "axios"
 import Footer from "./Footer"
 import Header from "./Header";
 
 const Home = () => {
-    const [priceData, setPriceData] = useState([{
-        "id": 4,
-        "category": "Sonstiges",
-        "description": "Essen",
-        "price": 220,
-        "created_at": "2020-10-09T22:00:00.000Z",
-        "user_id": 1
-    },
-    {
-        "id": 5,
-        "category": "Sonstiges",
-        "description": "Saft",
-        "price": 20,
-        "created_at": "2020-10-09T22:00:00.000Z",
-        "user_id": 1
-    },
-    {
-        "id": 6,
-        "category": "Sonstiges",
-        "description": "test description",
-        "price": 200,
-        "created_at": "2020-10-09T22:00:00.000Z",
-        "user_id": 1
-    }])
-    console.log(setPriceData)
+    // let { id } = useParams()
+    // const [priceData, setPriceData] = useState()
+
+    useEffect(() => {
+        axios.get("http://127.0.0.1:5000/balance")
+            .then(result => { console.log(result) })
+            .catch(err => console.log(err))
+    }, [])
+
+    // const deleteTransfer = () => {
+    //     axios.delete(`/input/${id}`)
+    //         .then(result => window.location.href = result.data.redirect)
+    //         .catch(err => console.log(err))
+    // }
+
     return (<>
         <Header />
-        <section>
-            <h4>Letzte Transaktionen</h4>
-            <p>Show full</p>
-            <ul>
-                {priceData.map((transfer) => <li key={transfer.id}>
+        <main>
+            <section>
+                <h4>Letzte Transaktionen</h4>
+                <p>Show full</p>
+                <ul>
+                    {/* {priceData.map((transfer) => <li key={transfer.id}>
                     <h4>{transfer.description}</h4>
                     <p>{transfer.created_at}</p>
                     <p>{transfer.price}</p>
-                </li>)}
-            </ul>
-            <button disabled="disabled">Mehr Transaktionen</button>
-        </section>
+                </li>)} */}
+                </ul>
+                <button disabled="disabled">Mehr Transaktionen</button>
+            </section>
+        </main>
         <Footer />
     </>);
 }
--- frontend/src/components/Turnovers.js
+++ frontend/src/components/Turnovers.js
@@ -25,32 +25,37 @@ const Turnovers = () => {
     });
 
     const newTransfer = () => {
-        axios.post(`/api`, inputs)
+        axios.post(`/input`, inputs)
             .then(result => window.location.href = result.data.redirect)
             .catch(err => console.log(err))
     }
 
     return (
-        <section>
-            <Link to="/home"><img src="../img/shape.png" alt="#" /></Link> 
-            <h1>Umsätze</h1>
-            <div id="success">
-                <img src="../img/sucess.png" alt="" />
-                <h3><span>Erfolgreich</span><br />eingetragen!</h3>
-                <p><span>Datum</span><br />{data.created_at}</p>
-                <p><span>Zeit</span><br />{data}</p>
-                <p><span>Kategorie</span><br />{data.category}</p>
-                <h3><span>Summe</span><br />{data.price}</h3>
-            </div>
-            <form>
-                <select onChange={(event) => setData(event.target.value)} required>{valueChoice}</select>
-                <input type="text" name="description" placeholder="Beschreibung" value={inputs.description} onChange={handleInputs} required />
-                <input type="number" name="price" placeholder="Geldbetrag" value={inputs.price} onChange={handleInputs} required />
-                <input type="created_at" name="created_at" placeholder="Datum" value={inputs.created_at} onChange={handleInputs} required />
-                <input type="submit" value="Abschicken" onClick={newTransfer} />
-            </form>
-            <Footer/>
-        </section>
+        <>
+            <Header />
+            <main>
+                <section>
+                    <Link to="/home"><img src="../img/shape.png" alt="#" /></Link>
+                    <h1>Umsätze</h1>
+                    <div id="success">
+                        <img src="../img/sucess.png" alt="" />
+                        <h3><span>Erfolgreich</span><br />eingetragen!</h3>
+                        <p><span>Datum</span><br />{data.created_at}</p>
+                        <p><span>Zeit</span><br />{data}</p>
+                        <p><span>Kategorie</span><br />{data.category}</p>
+                        <h3><span>Summe</span><br />{data.price}</h3>
+                    </div>
+                    <form>
+                        <select onChange={(event) => setData(event.target.value)} required>{valueChoice}</select>
+                        <input type="text" name="description" placeholder="Beschreibung" value={inputs.description} onChange={handleInputs} required />
+                        <input type="number" name="price" placeholder="Geldbetrag" value={inputs.price} onChange={handleInputs} required />
+                        <input type="created_at" name="created_at" placeholder="Datum" value={inputs.created_at} onChange={handleInputs} required />
+                        <input type="submit" value="Abschicken" onClick={newTransfer} />
+                    </form>
+                </section>
+            </main>
+            <Footer />
+        </>
     );
 };
 
