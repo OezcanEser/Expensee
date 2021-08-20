af79408ffd128cb20c1f9bf904746d448bfdcfaf
OezcanEser <oe.eser84@gmail.com>
2021-08-20 09:56:43 +0200

    new pull

--- frontend/src/components/PieChart.js
+++ frontend/src/components/PieChart.js
@@ -1,23 +1,11 @@
-import { Doughnut } from 'react-chartjs-2'
+import { Pie } from 'react-chartjs-2';
 import { useEffect, useState } from 'react';
 import axios from 'axios'
+
 const PieChart = () => {
     const [totalCosts, setTotalCosts] = useState(null);
-
-    const setPie = {
-        labels: ["Einnahmen", "Ausgaben", "Sparen", "Sonstiges"],
-        datasets: [{
-            label: "# of votes",
-            data: pieces(),
-            backgroundColor: [
-                "rgba(246, 53, 53, 1)",
-                "rgba(81, 95, 235, 1)",
-                "rgba(247, 199, 53, 1)",
-                "rgba(149, 152, 154, 1)",
-            ],
-            borderWidth: 0,
-        }],
-    }
+    const showData = totalCosts + "%";
+    console.log(showData)
 
     useEffect(() => {
         async function getTotalCosts() {
@@ -27,7 +15,6 @@ const PieChart = () => {
         }
         getTotalCosts();
     }, []);
-
     function pieces() {
         if (totalCosts) {
             return Object.keys(totalCosts).map(element => {
@@ -37,44 +24,52 @@ const PieChart = () => {
             })
         }
     }
-    console.log(pieces())
-
-
-    const chartSparen = new Number();
-    chartSparen.src = totalCosts ? totalCosts.sparen : null
-
-    const test = {
-        beforeDraw: (chart) => {
-            if (chartSparen.complete) {
-                const ctx = chart.ctx;
-                const { top, left, width, height } = chart.chartArea;
-                const x = left + width / 2 - chartSparen.width / 2;
-                const y = top + height / 2 - chartSparen.height / 2;
-                ctx.drawChart(chartSparen, x, y);
-            } else {
-                chartSparen.onload = () => chart.draw();
-            }
-        }
-    }
-    return (<>
-        <Doughnut
-            data={setPie}
-            options={{
-                plugins: [test],
-                plugins: {
-                    tooltip: {
-                        enabled: false
+    // {totalCosts && totalCosts.sparen}
+    return (
+        <div className="chart">
+            <div className="chartText">
+                <p>{totalCosts && totalCosts.sparen}€</p>
+            </div>
+            <Pie
+                data={{
+                    labels: ["Einnahmen", "Ausgaben", "Sparen", "Sonstiges"],
+                    datasets: [{
+                        label: "# of votes",
+                        data: pieces(),
+                        backgroundColor: [
+                            "rgba(246, 53, 53, 1)",
+                            "rgba(81, 95, 235, 1)",
+                            "rgba(247, 199, 53, 1)",
+                            "rgba(149, 152, 154, 1)",
+                        ],
+                        borderWidth: 0,
                     },
-                    legend: {
-                        position: "bottom",
-                        labels: {
-                            color: "#ffffff",
-                            usePointStyle: true,
+                    {
+                        data: [1],
+                        backgroundColor: [
+                            "white"
+                        ],
+                        borderWidth: 0,
+                    }
+                    ]
+                }}
+                options={{
+                    plugins: {
+                        tooltip: {
+                            enabled: false
+                        },
+                        legend: {
+                            position: "bottom",
+                            labels: {
+                                color: "#ffffff",
+                                usePointStyle: true,
+                            }
                         }
                     }
                 }
-            }}
-        /></>);
+                }
+            />
+        </div>)
 }
 
-export default PieChart;
+export default PieChart
