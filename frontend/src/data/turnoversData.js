117ade6974628632f56cdc6a05ed498486256ad7
matijaskorup1991 <smatija1991@gmail.com>
2021-08-23 19:29:49 +0200

    footer history

---
+++ frontend/src/data/turnoversData.js
@@ -0,0 +1,9 @@
+export const optionData = [' ', 'Einnahmen', 'Ausgaben', 'Sonstiges'];
+export const descriptionData = [
+  'Gehalt',
+  'Lebensmittel',
+  'Shopping',
+  'Wohnung',
+  'Restaurant',
+  'Versicherung',
+];
