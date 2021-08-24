20f1c66d728b45a2379a1974982eee722d8740bf
OezcanEser <oe.eser84@gmail.com>
2021-08-24 10:49:29 +0200

    fix

--- backend/server.js
+++ backend/server.js
@@ -29,14 +29,14 @@ app.use((req, res, next) => {
 //   app.get('*', (req, res) =>    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))  )}
 
 //custom error handler
-app.use((error, req, res, next) => {
-  if (res.headerSent) {
-    return next(error);
-  }
-  res.status(error.code || 500).json({
-    message: error.message || 'Unknown Error!',
-  });
-});
+// app.use((error, req, res, next) => {
+//   if (res.headerSent) {
+//     return next(error);
+//   }
+//   res.status(error.code || 500).json({
+//     message: error.message || 'Unknown Error!',
+//   });
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
