4cfd6f83ab53d3a76efd244215d99b4c40b16411
OezcanEser <oe.eser84@gmail.com>
2021-08-25 09:43:48 +0200

    newstuff

--- backend/server.js
+++ backend/server.js
@@ -21,14 +21,30 @@ app.get('*', (req, res) => {
 });
 
 // custom error handler
-// app.use((error, req, res, next) => {
-//   if (res.headerSent) {
-//     return next(error);
-//   }
-//   res.status(error.code || 500).json({
-//     message: error.message || 'Unknown Error!',
-//   });
-// });
+app.use("/user", (error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500).json({
+    message: error.message || 'Unknown Error!',
+  });
+});
+app.use("/input", (error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500).json({
+    message: error.message || 'Unknown Error!',
+  });
+});
+app.use("/balance", (error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500).json({
+    message: error.message || 'Unknown Error!',
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
